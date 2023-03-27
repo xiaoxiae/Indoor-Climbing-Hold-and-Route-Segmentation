bd7d70708d8fca2372f243430e86133f0423a533
xiaoxiae <tomas@slama.dev>
2023-03-27 14:02:48 +0200

    STD: move combined.py code to utils.py

--- std/combined.py
+++ std/combined.py
@@ -1,8 +1,6 @@
 from utils import *
 
 
-THRESHOLD_STEP = 5
-
 img = cv.imread('298.jpg')
 
 img_copy = img.copy()
@@ -20,46 +18,7 @@ contours = simplify_contours(contours)
 contours = filter_size_contours(contours)
 contours = filter_straight_contours(contours)
 
-# pre-compute thresholds and contours/edges since they're used repeatedly
-thresholds = {}
-for i in range(0, 255, THRESHOLD_STEP):
-    t = threshold(blur, i, 255)
-    thresholds[i] = []
-
-    for c_t in t[:,:,0], t[:,:,1], t[:,:,2]:
-        t_edges = canny(c_t)
-        t_contours = find_contours(t_edges)
-        t_contours = simplify_contours(t_contours)
-
-        thresholds[i].append((t_edges, t_contours))
-
-hold_approximations = {}
-for k in keypoints:
-    nearby_contours = get_nearby_contours(k.pt, contours, (k.size / 2))
-
-    if len(nearby_contours) == 0:
-        continue
-
-    best_contour = None
-    best_contour_error = float('inf')
-
-    for i in range(0, 255, THRESHOLD_STEP):
-        for t_edges, t_contours in thresholds[i]:
-            closest_t_contour = get_closest_contour(k.pt, t_contours)
-
-            if closest_t_contour is None:
-                continue
-
-            err = squared_contour_error(nearby_contours, closest_t_contour)
-
-            if best_contour_error > err:
-                best_contour_error = err
-                best_contour = closest_t_contour
-
-    if best_contour is None:
-        continue
-
-    hold_approximations[k] = best_contour
+hold_approximations = detect_holds(img, keypoints, contours)
 
 img_copy = img.copy()
 draw_contour_boxes(img_copy, list(hold_approximations.values()), color=(0, 255, 0))
--- std/utils.py
+++ std/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import cv2 as cv
 import numpy as np
+import detectron2
 from scipy.stats import linregress
 from shapely.geometry import Polygon
 
@@ -10,6 +11,8 @@ STROKE_THICKNESS = 5
 SAVE = True
 BLUR_SIZE = 13
 CANNY = (20, 25)
+THRESHOLD_STEP = 5
+
 
 def dist(p1, p2):
     """Return the Euclidean distance between p1 and p2."""
@@ -324,3 +327,56 @@ def squared_contour_error(contours_from, contour_to):
             contour_error += point_to_contour_distance(pc, contour_to)
 
     return contour_error / point_count
+
+def detect_holds(img, keypoints, contours):
+    """Detect holds by combining blob and edge detection."""
+    # pre-compute thresholds and contours/edges since they're used repeatedly
+    blur = gaussian_blur(img)
+
+    thresholds = {}
+    for i in range(0, 255, THRESHOLD_STEP):
+        t = threshold(blur, i, 255)
+        thresholds[i] = []
+
+        for c_t in t[:,:,0], t[:,:,1], t[:,:,2]:
+            t_edges = canny(c_t)
+            t_contours = find_contours(t_edges)
+            t_contours = simplify_contours(t_contours)
+
+            thresholds[i].append((t_edges, t_contours))
+
+    hold_approximations = {}
+    for k in keypoints:
+        nearby_contours = get_nearby_contours(k.pt, contours, (k.size / 2))
+
+        if len(nearby_contours) == 0:
+            continue
+
+        best_contour = None
+        best_contour_error = float('inf')
+
+        # find optimal threshold
+        for i in range(0, 255, THRESHOLD_STEP):
+            for t_edges, t_contours in thresholds[i]:
+                closest_t_contour = get_closest_contour(k.pt, t_contours)
+
+                if closest_t_contour is None:
+                    continue
+
+                err = squared_contour_error(nearby_contours, closest_t_contour)
+
+                if best_contour_error > err:
+                    best_contour_error = err
+                    best_contour = closest_t_contour
+
+        if best_contour is None:
+            continue
+
+        hold_approximations[k] = best_contour
+
+    return hold_approximations
+
+def to_detectron_format(contours):
+    """Convert the contours of holds to a format that is parsable by detectron.
+    https://detectron2.readthedocs.io/en/latest/tutorials/models.html#model-output-format"""
+    pass
